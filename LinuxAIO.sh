bc7a2b6b168545971c934d07f32b4e8a735f513b
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-14 13:52:52 -0700

    Update LinuxAIO.sh
    written 3 lines

--- LinuxAIO.sh
+++ LinuxAIO.sh
@@ -1,1 +1,10 @@
+#tester
+echo ""
+echo "Welcome to NadekoBot. Downloading the latest installer..."
+root=$(pwd)
+wget -N https://raw.githubusercontent.com/HokutoChen/NadekoScript/main/nadeko_pi_master_installer.sh
 
+bash nadeko_pi_master_installer.sh
+cd "$root"
+rm "$root/nadeko_pi_installer.sh"
+exit 0
