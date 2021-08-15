b02b58855ffb45ff87593f16da7a3d0ec1fff20d
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-15 10:14:21 -0700

    Update n-menu.sh

--- n-menu.sh
+++ n-menu.sh
@@ -29,40 +29,45 @@ while [ $choice -eq 9 ]; do
       	read choice
       
 
-	if [ $choice -eq 1 ]; then
+	if [[ $choice -eq 1 ]]; then
       		echo ""
 		echo "Downloading the prerequisites installer script"
 		wget -N "$base_url/$script_prerequisites" && bash "$root/$script_prerequisites"
 		echo ""
 		choice=9
-	elif [ $choice -eq 2 ]; then
+	elif [[ $choice -eq 2 ]]; then
                 echo ""
 		echo "Downloading the NadekoBot installer script"
 		wget -N "$base_url/$script_install" && bash "$root/$script_install"
 		echo ""
 		sleep 2s
 		choice=9
-	elif [ $choice -eq 3 ]; then
+	elif [[ $choice -eq 3 ]]; then
                 echo ""
 		echo "Downloading the NadekoBot run script"
 		wget -N "$base_url/$script_run" && bash "$root/$script_run"
 		echo ""
 		sleep 2s
 		bash "$root/linuxAIO.sh"
-	elif [ $choice -eq 4 ]; then
+	elif [[ $choice -eq 4 ]]; then
               	echo ""
 		echo "Downloading the NadekoBot run and auto restart script"
 		wget -N "$base_url/$script_arn" && bash "$root/$script_arn"
 		echo ""
 		sleep 2s
 		bash "$root/linuxAIO.sh"
-	elif [ $choice -eq 5 ]; then
+	elif [[ $choice -eq 5 ]]; then
 		echo ""
 		echo "Downloading Credentials setup script"
 		wget -N "$base_url/$script_credentials" && bash "$root/$script_credentials"
 		echo ""
 		sleep 2s
 		bash "$root/linuxAIO.sh"
+	elif [[ $choice -eq 6 ]] ; then
+		echo ""
+		echo "Exiting..."
+		cd "$root"
+		exit 0
 	else
 		echo "Invalid choice"
 		echo ""
