758621f7eae91a8e4c4f2f87f3e91460b4dad3db
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-14 17:51:25 -0700

    Update linuxAIO.sh

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Welcome to NadekoBot. Downloading the latest installer..."
 root=$(pwd)
-wget -N https://github.com/HokutoChen/NadekoScript/blob/08bfbfaf0210eed700952c0e3e4b5b7186ca7be2/n-menu.sh
+wget -N https://raw.githubusercontent.com/HokutoChen/NadekoScript/08bfbfaf0210eed700952c0e3e4b5b7186ca7be2/n-menu.sh
 
 bash n-menu.sh
 cd "$root"
