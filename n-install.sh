2491864302b6befc29466b974293b068cad47610
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-14 17:30:19 -0700

    Create n-install.sh

---
+++ n-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+root=$(pwd)
+
+# todo data handling
+git clone https://gitlab.com/kwoth/nadekobot -b v3 --depth 1
+cd nadekobot
+
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+dotnet build src/NadekoBot/NadekoBot.csproj -c Release -o output/
+
+cd "$root"
+rm "$root/n-install.sh"
+exit 0
