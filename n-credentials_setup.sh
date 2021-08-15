f553f9dde45d8e3cde5804def35370af0b85cdce
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-15 16:51:09 -0700

    Update n-credentials_setup.sh

--- n-credentials_setup.sh
+++ n-credentials_setup.sh
@@ -17,6 +17,15 @@ done
 cd $HOME/nadekobot/src/NadekoBot
 mv credentials.json credentials.json.old
 
+echo Please enter your bot client ID:
+echo ""
+read clientid
+echo ""
+echo Alright saved '$clientid' as your client ID.
+echo ""
+echo "----------"
+echo ""
+
 echo "Please enter your bot token \n(It is not bot secret, it should be ~59 characters long.):"
 echo ""
 read token
